980d3f64186536886be580ca974573f364c29acc
ryu-k67 <200441067@ccmailg.meijo-u.ac.jp>
2020-11-20 23:01:46 +0900

    枠修正

--- clock_border.js
+++ clock_border.js
@@ -1,7 +1,8 @@
-function border()
-{
-    var obj=document.getElementById();
-    
-    obj.style.border="solid 3px black";
-    
-}
+function changeBorder(idname){
+    var obj = document.getElementById(idname);
+    if(obj.style.borderWidth == "3px"){
+      obj.style.borderWidth = "0px";
+    }else{
+      obj.style.border = "3px solid blue";
+    }
+  }
